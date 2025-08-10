e9f4df6a7eb4ceacf959e9a6642d8b753876ea44
Bankole David <bankydavid12@gmail.com>
2025-08-10 04:01:26 +0100

    fixed faq component error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Download from './components/Download';
-import Faq from './components/Faq.JSX';
+import Faq from './components/Faq';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
